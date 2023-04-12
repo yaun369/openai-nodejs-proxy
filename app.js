cea811b49c256e962f8c190cc7c894eb9d285d92
ymm <geekreading@163.com>
2023-04-12 14:50:06 +0800

    fix: 修改端口

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
-const port = 9000;
+const port = 3000;
 
 app.use(
   "/",
