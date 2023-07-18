bf90418454bdf48a3d16ae6d71d48ccce4070322
ymm <geekreading@163.com>
2023-07-18 13:47:18 +0800

    fix: 新增arxiv地址

--- app.js
+++ app.js
@@ -4,6 +4,38 @@ const app = express();
 const port = 3000;
 
 app.use(
+  "/arxiv/api",
+  createProxyMiddleware({
+    target: "http://export.arxiv.org/api/query",
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+      proxyReq.removeHeader("x-forwarded-for");
+      proxyReq.removeHeader("x-real-ip");
+    },
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
+
+app.use(
+  "/arxiv",
+  createProxyMiddleware({
+    target: "https://arxiv.org",
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+      proxyReq.removeHeader("x-forwarded-for");
+      proxyReq.removeHeader("x-real-ip");
+    },
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
+
+app.use(
   "/",
   createProxyMiddleware({
     target: "https://api.openai.com",
