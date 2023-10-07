c8d46bb1676c8bcb33db578a5d960f730c1daac9
ymm <geekreading@163.com>
2023-10-07 13:40:40 +0800

    fix: 新增anthropic接口代理

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const app = express();
 const port = 3000;
 
 app.use(
+  "/anthropic",
+  createProxyMiddleware({
+    target: "https://api.anthropic.com",
+    pathRewrite: {
+      "^/anthropic": "", // 将路径中的 /anthropic 替换为空字符串
+    },
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+      proxyReq.removeHeader("x-forwarded-for");
+      proxyReq.removeHeader("x-real-ip");
+    },
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
+
+app.use(
   "/arxiv/api",
   createProxyMiddleware({
     target: "http://export.arxiv.org/api/query",
